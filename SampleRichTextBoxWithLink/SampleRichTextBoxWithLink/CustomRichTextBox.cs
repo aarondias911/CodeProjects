faecc1c64ae9e7841780609914a35e56103447eb
aarondias911 <aarondias911@gmail.com>
2017-01-13 15:03:28 +0530

    renaming project

--- SampleRichTextBoxWithLink/SampleRichTextBoxWithLink/CustomRichTextBox.cs
+++ SampleRichTextBoxWithLink/SampleRichTextBoxWithLink/CustomRichTextBox.cs
@@ -8,7 +8,7 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Documents;
 
-namespace SampleRichTextBoxWithLink
+namespace RichTextBoxWithLink
 {
     public class CustomRichTextBox : RichTextBox
     {
